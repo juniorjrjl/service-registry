33ddac9e25b94da810f59d45d466a86c822ab798
Junior <junior.jr.jl@gmail.com>
2022-10-28 13:15:01 -0300

    atualização da classe de auth

--- src/main/java/com/ead/serviceregistry/config/WebSecurityConfig.java
+++ src/main/java/com/ead/serviceregistry/config/WebSecurityConfig.java
@@ -3,14 +3,15 @@ package com.ead.serviceregistry.config;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
-import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.core.userdetails.User;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.provisioning.InMemoryUserDetailsManager;
+import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
-public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
+public class WebSecurityConfig {
 
     private final String username;
     private final String password;
@@ -21,22 +22,26 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
         this.password = password;
     }
 
-    @Override
-    protected void configure(final HttpSecurity http) throws Exception {
+    @Bean
+    public SecurityFilterChain filterChain(final HttpSecurity http) throws Exception {
         http
                 .httpBasic()
                 .and()
                 .authorizeRequests()
                 .anyRequest().authenticated()
-                .and().csrf().disable().formLogin();
+                .and()
+                .csrf().disable()
+                .formLogin();
+        return http.build();
     }
 
-    @Override
-    protected void configure(final AuthenticationManagerBuilder auth) throws Exception {
-        auth.inMemoryAuthentication()
-                .withUser(username)
+    @Bean
+    public InMemoryUserDetailsManager userDetailsService() {
+        var user = User.withUsername(username)
                 .password(passwordEncoder().encode(password))
-                .roles("ADMIN");
+                .roles("ADMIN")
+                .build();
+        return new InMemoryUserDetailsManager(user);
     }
 
     @Bean
