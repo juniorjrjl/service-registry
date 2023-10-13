314d68fe0bb41d20e3e6b9c0ff2d6d1a96434c5c
Junior <junior.jr.jl@gmail.com>
2023-10-13 15:23:27 -0300

    feat: ajuste de atualização de lib

--- Dockerfile
+++ Dockerfile
@@ -1,4 +1,4 @@
-FROM gradle:7.5.1-jdk17
+FROM gradle:8.1.1-jdk17
 
 RUN apt-get update && apt-get install -qq -y --no-install-recommends
 
--- src/main/java/com/ead/serviceregistry/config/WebSecurityConfig.java
+++ src/main/java/com/ead/serviceregistry/config/WebSecurityConfig.java
@@ -3,12 +3,16 @@ package com.ead.serviceregistry.config;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
 import org.springframework.security.core.userdetails.User;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 import org.springframework.security.web.SecurityFilterChain;
+
+import static jakarta.servlet.DispatcherType.ERROR;
 
 @Configuration
 public class WebSecurityConfig {
@@ -24,15 +28,13 @@ public class WebSecurityConfig {
 
     @Bean
     public SecurityFilterChain filterChain(final HttpSecurity http) throws Exception {
-        http
-                .httpBasic()
-                .and()
-                .authorizeRequests()
-                .anyRequest().authenticated()
-                .and()
-                .csrf().disable()
-                .formLogin();
-        return http.build();
+        return http
+                .httpBasic(Customizer.withDefaults())
+                .authorizeHttpRequests(auth -> auth
+                    .dispatcherTypeMatchers(ERROR).permitAll()
+                    .anyRequest().authenticated())
+                .csrf(AbstractHttpConfigurer::disable)
+                .formLogin(Customizer.withDefaults()).build();
     }
 
     @Bean
